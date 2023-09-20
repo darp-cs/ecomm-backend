14aa47844a2365947f654ece970d6b346784a934
Diego Rodriguez <darp@Diegos-MBP.attlocal.net>
2023-09-20 00:12:26 -0400

    Added cross origin to auth endpoint

--- ecomm/src/main/java/com/ecomm/Controller/AuthController.java
+++ ecomm/src/main/java/com/ecomm/Controller/AuthController.java
@@ -11,6 +11,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,6 +26,7 @@ import com.ecomm.Service.UserService;
 
 //Rest controller annotation marks class as a Rest controller that will handle apis
 //Request Mapping requires apis to be preceded with /auth (eg. /auth/login)
+@CrossOrigin
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
